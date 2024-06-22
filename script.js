57ef0f326b54c73a37fd583fc9c499de2a979091
Brayan Casallas <beto980730@gmail.com>
2024-06-21 19:57:31 -0500

    Actualiza

---
+++ script.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Juego del número secreto";
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Selecciona un número de 1 a 10";
